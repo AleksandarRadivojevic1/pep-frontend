85a8d70a011e9ab740b33373b0d2eed5e2b263fe
Aleksandar Radivojevic <radivojevicaleksandar64@gmail.com>
2024-08-23 17:25:47 +0200

    Add Fonts Awesome and basic Navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AlbumView from '@/views/AlbumView.vue'
+import ArtistView from '@/views/ArtistView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,16 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    //{
+    //   path: '/albums',
+    //   name: 'albums',
+    //   component: AlbumView
+    // },
+    // {
+    //   path: '/artists',
+    //   name: 'artists',
+    //   component: ArtistView
+    // },
   ]
 })
 
